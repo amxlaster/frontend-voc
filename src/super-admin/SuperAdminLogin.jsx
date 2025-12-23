6d0631dba179589b445e0b04aa38052c0cb7d270
amxlaster <animmasrilaster@gmail.com>
2025-12-23 13:21:26 +0530

    Update SuperAdminLogin.jsx

--- src/super-admin/SuperAdminLogin.jsx
+++ src/super-admin/SuperAdminLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../lib/api";   // ⭐ IMPORTANT
+import API from "../lib/api";          // ⭐ IMPORTANT
 import logo from "../assets/Arumexa Logo.png";
 import "./SuperAdminLogin.css";
 
